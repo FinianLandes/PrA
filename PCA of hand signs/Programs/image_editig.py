db754ef25c73f6bac627922842f3f1858ba4e7a1
Chaerne <finianlandes@gmail.com>
2024-10-21 09:23:32 +0200

    Added Full Dataset, Added Test dataset of diff. hand

--- PCA of hand signs/Programs/image_editig.py
+++ PCA of hand signs/Programs/image_editig.py
@@ -13,17 +13,17 @@ def black_white(img) -> Image:
     return img.convert('L')
 
 _debug: bool = True
-image_dir: str = "PCA of hand signs//Images//5_HF_M_P"
-image_dest: str = "PCA of hand signs//Processed Images"
+image_dir: str = "PCA of hand signs//Images//3_O_P_C_Pi_HF_E_M_T_F_I"
+image_dest: str = "PCA of hand signs//Processed Images//Test"
 
-order_names: list = ["high_five", "metal", "peace"]#Abbr. High Five: HF, Metal: M, Peace: P
-n_per_cat: int = 5
+order_names: list = ["okay", "peace", "crossed", "pistol", "high_five", "easy", "metal", "thumbs_up", "fist", "index"]#Abbr. High Five: HF, Metal: M, Peace: P, Crossed: C, Pistol: Pi, Easy: E, Thumbs up: T, Fist: F, Index: I, Okay: O
+n_per_cat: int = 3
 replace: bool = True # if False checks whether name already exists, if it does it increments the number and adds the new image
 resolution: int = 256 #resulting image in format resolution x resolution
 file_names: list = sorted(os.listdir(image_dir))
 
 for i in range(len(file_names) // n_per_cat):
-    image_num: list = [(i * 5), (i * 5) + 5] #Images [0,5)
+    image_num: list = [(i * n_per_cat), (i * n_per_cat) + n_per_cat] #Images [0,5)
     images: list = file_names[image_num[0]:image_num[1]]
     image_name: str = order_names[i]
     for j, name in enumerate(images):
@@ -40,7 +40,7 @@ for i in range(len(file_names) // n_per_cat):
                     img_name = image_name + "_" + f"{n:02}"
                 else: break
         if _debug:
-            print("Saved Image as: " + img_name)
+            print(str((i * n_per_cat) + j + 1) + "/" + str(len(file_names)) +" Saved Image as: " + img_name)
         img = rename_save_img(img, img_name , image_dest)
 
 
