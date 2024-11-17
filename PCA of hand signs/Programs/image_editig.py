dcc479d127925bf27be2e0f1441db0e9bed6623c
Chaerne <finianlandes@gmail.com>
2024-11-17 12:33:43 +0100

    Renamed and commented functions

--- PCA of hand signs/Programs/image_editig.py
+++ PCA of hand signs/Programs/image_editig.py
@@ -6,8 +6,8 @@ def rename_save_img(img: Image, name: str, path: str) -> None:
 def rotate(img: Image, alpha: int) -> Image:
     img = img.rotate(angle=alpha)
     return img
-def downscale(img: Image, resolution: int) -> Image:
-    new_size: list = (resolution, resolution)
+def downscale(img: Image, size: int) -> Image:
+    new_size: list = (size, size)
     return img.resize(new_size, Image.LANCZOS)
 def black_white(img: Image) -> Image:
     return img.convert('L')
@@ -21,7 +21,7 @@ image_dest: str = "PCA of hand signs//Processed Images//Train"
 order_names: list = ["thumbs_up", "easy", "peace", "pistol","metal"]#Abbr. High Five: HF, Metal: M, Peace: P, Crossed: C, Pistol: Pi, Easy: E, Thumbs up: T, Fist: F, Index: I, Okay: O
 n_per_cat: int = 10
 replace: bool = False # if False checks whether name already exists, if it does it increments the number and adds the new image
-resolution: int = 256 #resulting image in format resolution x resolution
+size: int = 256 #resulting image in format size x size
 file_names: list = sorted(os.listdir(image_dir))
 
 for i in range(len(file_names) // n_per_cat):
@@ -32,7 +32,7 @@ for i in range(len(file_names) // n_per_cat):
         img_name: str = image_name + "_" + f"{j:02}"
         img: Image = Image.open(image_dir + "//" + name)
         img = rotate(img, 180)
-        img = downscale(img, resolution)
+        img = downscale(img, size)
         img = black_white(img)
         if not replace:
             n: int = j
